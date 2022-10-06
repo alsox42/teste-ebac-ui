2dec950c1292141154399ef89de534192a5389f2
alsox369 <alccpe@yahoo.com.br>
2022-10-06 17:24:13 -0300

    alterações no teste automatizado

---
+++ cypress/integration/produto.spec.js
@@ -0,0 +1,32 @@
+///<reference types="cypress" />
+var faker = require('@faker-js/faker');
+
+context('Funcionalidade pagina de Produto', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produto/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last() 
+            //.eq(3) 
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+    });
+
+    it('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 2
+
+        cy.get('[class="product-block grid"]')
+        .contains('Ariel Roll Sleeve Sweatshirt')
+        .click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' x "Ariel Roll Sleeve Sweatshirt"')
+    })
+});
